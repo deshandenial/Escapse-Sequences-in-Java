c8e5799d6f7f9d924fd0d290785f7288a1f38dd3
deshanr <deshanr@QAPPS02.QAPPSTECH.IN>
2019-09-10 17:36:45 +0530

    commit

---
+++ Escapse Sequences in Java/src/escapse_sequences/Escapse_Sequences_class.java
@@ -0,0 +1,19 @@
+package escapse_sequences;
+
+public class Escapse_Sequences_class
+{
+
+	public static void main(String[] args) {
+		System.out.println("\n Hello World");
+		System.out.println("\r Hello World");
+		System.out.println("\f Hello World");
+		System.out.println("\b Hello World");
+		//System.out.println("\s Hello World");
+		System.out.println("\t Hello World");
+		System.out.println("\"Hello World");
+		System.out.println("\'Hello World");
+		/*System.out.println("\\Hello World");
+		System.out.println("\\ddd Hello World");
+		System.out.println("\\uxxx Hello World");*/
+	}
+}
